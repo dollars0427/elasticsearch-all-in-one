614924d205cbdabc095da59d0ca7bb6c61dfc53c
Sardo <sardo.ip@sardo.work>
2015-10-08 15:55:53 +0800

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,8 @@
 name             'elasticsearch-all-in-one'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures elasticsearch-all-in-one'
+maintainer       'Sardo'
+maintainer_email 'sardo.ip@sardo.work'
+license          'Apache-2.0'
+description      'A cookbook of elasticsearch with kibana and td-agent.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
