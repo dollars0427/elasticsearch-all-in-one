1c45220f1e442475b3208e15223fba400992207d
Sardo <sardo.ip@sardo.work>
2015-10-08 17:44:16 +0800

    Change "" to ''

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,11 @@
-#
+# encoding: UTF-8
 # Cookbook Name:: elasticsearch-all-in-one
 # Recipe:: default
 
-include_recipe "elasticsearch-all-in-one::_install_java"
+include_recipe 'elasticsearch-all-in-one::_install_java'
 
-include_recipe "elasticsearch-all-in-one::_install_elasticsearch"
+include_recipe 'elasticsearch-all-in-one::_install_elasticsearch'
 
-include_recipe "elasticsearch-all-in-one::_install_kibana"
+include_recipe 'elasticsearch-all-in-one::_install_kibana'
 
-include_recipe "elasticsearch-all-in-one::_install_td-agent"
+include_recipe 'elasticsearch-all-in-one::_install_td-agent'
